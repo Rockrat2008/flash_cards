231b54503787b461a20f68660c72b6a98e67a2f3
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-21 19:15:27 -0400

    Updates the tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,5 +2,23 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
+    it 'exists' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
     
+        expect(turn).to be_instance_of(Turn)
+    end
+
+    it 'has a card' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+    
+        expect(turn.card).to be_instance_of(Card)
+    end
+
+    it 'has a guess' do
+        card = turn = Turn.new("Juneau", card)
+    
+        expect(turn.guess).to eq("Juneau")
+    end
 end
