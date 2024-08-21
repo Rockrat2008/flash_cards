c72ab67a5efdedeabadc7bd36a28dd5032b0ad06
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-21 18:39:20 -0400

    Adds the turn.rb file

---
+++ lib/turn.rb
@@ -0,0 +1,22 @@
+class Turn
+    def initialize(guess, card)
+        @guess = answer
+        @card = card
+    end
+
+    def guess
+        @guess
+    end
+
+    def card
+
+    end
+
+    def correct?
+
+    end
+
+    def feedback
+        
+    end
+end
