b4ccd2708cff3c2b18cd6bb3debfa6babbd04a89
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-22 14:01:42 -0400

    Adds the deck.rb file with the starter code for the deck.

---
+++ lib/deck.rb
@@ -0,0 +1,20 @@
+class Deck
+    def initialize(cards)
+        @deck = cards
+    end
+
+    def cards
+        @deck.each do |card|
+            puts card
+        end
+
+    end
+
+    def count
+    
+    end
+
+    def cards_in_category(category)
+        
+    end
+end
