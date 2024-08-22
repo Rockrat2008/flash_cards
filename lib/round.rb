637d1743511cb6fd3a5afabd3d9a5da9730141e2
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-22 19:20:59 -0400

    Builds the skeleton outline of the round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,33 @@
 class Round
-    def initialize()
-        
+    def initialize(deck)
+        @deck = deck        
+    end
+
+    def turns
+
+    end
+
+    def current_card
+
+    end
+
+    def take_turn
+
+    end
+
+    def number_correct
+
+    end
+
+    def number_correct_by_category(category)
+
+    end
+
+    def percent_correct
+
+    end
+
+    def percent_correct_by_category
+
     end
 end
