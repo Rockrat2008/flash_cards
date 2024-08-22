3c1a1e943cb4de51856e89553c2afc887560fdff
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-22 17:06:50 -0400

    Added a test on the card count in the array

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -19,4 +19,13 @@ RSpec.describe Deck do
     
         expect(deck.count).to eq(2)
     end
+       
+    it 'the "count" number of cards is incorrect' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        cards = [card_1, card_2]
+        deck = Deck.new(cards)
+    
+        expect(deck.count).not_to eq(3)
+    end
 end
