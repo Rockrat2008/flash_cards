2b81e5fb5e78820ca30a3362809ebe163228f501
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-22 16:57:03 -0400

    Updates deck_spec.rb with a new test for deck count

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Deck do
     
         expect(deck).to be_instance_of(Deck)
     end
+
+    it 'returns the "count" number of cards' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        cards = [card_1, card_2]
+        deck = Deck.new(cards)
+    
+        expect(deck.count).to eq(2)
+    end
 end
