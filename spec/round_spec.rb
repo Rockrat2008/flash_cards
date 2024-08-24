dccce73a8f8abb6f2caffa7d6995f745d6f896b8
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-24 18:20:10 -0400

    Corrected spelling errors

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -8,10 +8,9 @@ RSpec.describe Round do
         card_1 = Card.new("Lead actress in Pretty Woman?", "Julia Roberts", :Hollywood)
         card_2 = Card.new("What is the 3rd planet?", "Earth", :STEM)
         card_3 = Card.new("Lead actor from Pretty Woman?", "Richard Gere", :Hollywood)
-        card_4 = Card.new("What is the first planet?", "Mercury", :STEM)
+        card_4 = Card.new("What is the 1st planet?", "Mercury", :STEM)
         card_5 = Card.new("What is the 2nd planet?", "Venus", :STEM)
-        cards = [card_1, card_2, card_3, card_4, card_5]
-        deck = Deck.new(cards)
+        deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
         round = Round.new(deck)
 
         expect(round).to be_instance_of Round
@@ -21,10 +20,9 @@ RSpec.describe Round do
         card_1 = Card.new("Lead actress in Pretty Woman?", "Julia Roberts", :Hollywood)
         card_2 = Card.new("What is the 3rd planet?", "Earth", :STEM)
         card_3 = Card.new("Lead actor from Pretty Woman?", "Richard Gere", :Hollywood)
-        card_4 = Card.new("What is the first planet?", "Mercury", :STEM)
+        card_4 = Card.new("What is the 1st planet?", "Mercury", :STEM)
         card_5 = Card.new("What is the 2nd planet?", "Venus", :STEM)
-        cards = [card_1, card_2, card_3, card_4, card_5]
-        deck = Deck.new(cards)
+        deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
         round = Round.new(deck)
 
         expect(round.turns).to be_empty
@@ -34,12 +32,22 @@ RSpec.describe Round do
         card_1 = Card.new("Lead actress in Pretty Woman?", "Julia Roberts", :Hollywood)
         card_2 = Card.new("What is the 3rd planet?", "Earth", :STEM)
         card_3 = Card.new("Lead actor from Pretty Woman?", "Richard Gere", :Hollywood)
-        card_4 = Card.new("What is the first planet?", "Mercury", :STEM)
+        card_4 = Card.new("What is the 1st planet?", "Mercury", :STEM)
         card_5 = Card.new("What is the 2nd planet?", "Venus", :STEM)
-        cards = [card_1, card_2, card_3, card_4, card_5]
-        deck = Deck.new(cards)
+        deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
         round = Round.new(deck)
 
         expect(round.current_card).to eq(deck.cards[0])
+    end
+
+    it 'takes a new turn' do
+        card_1 = Card.new("Lead actress in Pretty Woman?", "Julia Roberts", :Hollywood)
+        card_2 = Card.new("What is the 3rd planet?", "Earth", :STEM)
+        card_3 = Card.new("Lead actor from Pretty Woman?", "Richard Gere", :Hollywood)
+        card_4 = Card.new("What is the 1st planet?", "Mercury", :STEM)
+        card_5 = Card.new("What is the 2nd planet?", "Venus", :STEM)
+        deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+        round = Round.new(deck)
+        guess = Turn.new(deck.cards[0], "Julia Roberts")
     end
 end
