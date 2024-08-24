880fcb71d043e2ac6d73886447262d0d269b7903
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-24 15:38:24 -0400

    Finished final test in deck_spec.rb file

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,5 +1,6 @@
 require './lib/card'
 require './lib/deck'
+require 'pry'
 
 RSpec.describe Deck do
     it 'exists' do
@@ -27,5 +28,19 @@ RSpec.describe Deck do
         deck = Deck.new(cards)
     
         expect(deck.count).not_to eq(3)
+    end
+
+    it 'pulls the correct category' do
+        card_1 = Card.new("Lead actress in Pretty Woman?", "Julia Roberts", :Hollywood)
+        card_2 = Card.new("What is the 3rd planet?", "Earth", :STEM)
+        card_3 = Card.new("Lead actor from Pretty Woman?", "Richard Gere", :Hollywood)
+        card_4 = Card.new("What is the first planet?", "Mercury", :STEM)
+        card_5 = Card.new("What is the 2nd planet?", "Venus", :STEM)
+        cards = [card_2, card_4, card_5]
+        deck = Deck.new(cards)
+
+        test_result = deck.cards_in_category(:STEM)
+
+        expect(test_result.length).to eq(3)
     end
 end
