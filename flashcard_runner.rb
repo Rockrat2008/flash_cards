5dd162fbfde290615413c4a079224d276e4a6bd3
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-24 18:19:21 -0400

    Updated runner file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,3 +1,16 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+card_1 = Card.new("Lead actress in Pretty Woman?", "Julia Roberts", :Hollywood)
+card_2 = Card.new("What is the 3rd planet?", "Earth", :STEM)
+card_3 = Card.new("Lead actor from Pretty Woman?", "Richard Gere", :Hollywood)
+card_4 = Card.new("What is the 1st planet?", "Mercury", :STEM)
+card_5 = Card.new("What is the 2nd planet?", "Venus", :STEM)
+deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+round = Round.new(deck)
+
 puts "Please enter your name:"
 player_name = gets
 puts "Welcome #{player_name.chomp}! You are playing with X cards."
