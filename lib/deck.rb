3423e317f3a4f5b97296641698d66fb8e442f82c
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-24 15:37:56 -0400

    Finishes final method in deck.rb file

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,9 @@
 class Deck
+    # attr_reader :@deck
+
     def initialize(cards)
         @deck = cards
+        @category_cards = []
     end
 
     def cards
@@ -11,10 +14,12 @@ class Deck
     end
 
     def count
-    
+        @deck.count
     end
 
     def cards_in_category(category)
-        
+        @deck.select do |card|
+            card.category == category
+        end
     end
 end
