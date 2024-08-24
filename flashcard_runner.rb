88cbe35e7fe63394b393946eb1bd2e52803a5ffc
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-24 17:18:00 -0400

    Updates program to request player name.

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,4 +1,6 @@
-puts "Welcome! You are playing with X cards."
+puts "Please enter your name:"
+player_name = gets
+puts "Welcome #{player_name.chomp}! You are playing with X cards."
 puts "------------------------------------"
 puts "This is card X out of X."
 puts "Question: Read question."
