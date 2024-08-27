1fb30012893f51b8e970c0c591335f8e14df8077
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-26 22:39:11 -0400

    Updated runner file to start game

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -11,9 +11,4 @@ card_5 = Card.new("What is the 2nd planet?", "Venus", :STEM)
 deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
 round = Round.new(deck)
 
-puts "Please enter your name:"
-player_name = gets
-puts "Welcome #{player_name.chomp}! You are playing with X cards."
-puts "------------------------------------"
-puts "This is card X out of X."
-puts "Question: Read question."
+play = round.start
