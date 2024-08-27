59a21f2df121473513f2c9de4605fd4c341f8cea
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-27 09:56:06 -0400

    Adds data to the cards array

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -8,7 +8,9 @@ card_2 = Card.new("What is the 3rd planet?", "Earth", :STEM)
 card_3 = Card.new("Lead actor from Pretty Woman?", "Richard Gere", :Hollywood)
 card_4 = Card.new("What is the 1st planet?", "Mercury", :STEM)
 card_5 = Card.new("What is the 2nd planet?", "Venus", :STEM)
-deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+card_6 = Card.new("Who is the lead actor in Top Gun?", "Tom Cruise", :Hollywood)
+card_7 = Card.new("What galaxy are we in?", "Milky Way", :STEM)
+deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6, card_7])
 round = Round.new(deck)
 
 play = round.start
