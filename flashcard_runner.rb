c7bb1c3300b1b7a47f0ab92740abf6d2d2386ec6
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-27 17:27:47 -0400

    Updated spelling in questions

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,9 +3,9 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-card_1 = Card.new("Lead actress in Pretty Woman?", "Julia Roberts", :Hollywood)
+card_1 = Card.new("Who is the lead actress in Pretty Woman?", "Julia Roberts", :Hollywood)
 card_2 = Card.new("What is the 3rd planet?", "Earth", :STEM)
-card_3 = Card.new("Lead actor from Pretty Woman?", "Richard Gere", :Hollywood)
+card_3 = Card.new("Who is the lead actor from Pretty Woman?", "Richard Gere", :Hollywood)
 card_4 = Card.new("What is the 1st planet?", "Mercury", :STEM)
 card_5 = Card.new("What is the 2nd planet?", "Venus", :STEM)
 card_6 = Card.new("Who is the lead actor in Top Gun?", "Tom Cruise", :Hollywood)
